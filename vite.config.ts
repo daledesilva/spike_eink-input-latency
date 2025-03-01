56d3f2e4ebd9d1d4e562cfc67b958a942a5ed268
Dale de Silva <daledesilva@gmail.com>
2025-03-02 01:04:11 +1100

    Added support for chrome (Only in https)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+///////////////
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  
   server: {
     host: '0.0.0.0', // This makes the server accessible publicly
     port: 3000
-  }
+  },
+
+  // This gives you a basic SSL certificate
+  // It's needed to enable getCoalesced function on Chrome
+  plugins: [
+    react(),
+    basicSsl({
+      name: 'test', // name of certification
+      domains: ['*.custom.com'], // custom trust domains
+      certDir: '/Users/.../.devServer/cert' // custom certification directory
+    })
+  ]
+
 })
